460645fe4e3bf76d0d06665729f535f893e829c0
Ekott2006 <84778161+Ekott2006@users.noreply.github.com>
2022-05-22 03:17:11 +0100

    Update index.js
    Working Version 1

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const SHORTURL = []
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.post("/api/shorturl", (request, response) => {
+  const url = request.body.url.split("/")[2]
+  return dns.lookup(url, function (err, addresses, family) {
+    if (err) {
+      return response.json({ error: 'invalid url' })
+    }
+    const json = { original_url: request.body.url, short_url:  SHORTURL.length+1}
+    SHORTURL.push(json)
+    response.json(json)
+  });
+})
+app.get("/api/shorturl/:url", (request, response) => {
+  const url = SHORTURL.filter(({short_url}) => short_url === Number(request.params.url))[0]
+  response.redirect(url.original_url)
+})
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
